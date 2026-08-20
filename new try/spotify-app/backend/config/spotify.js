470ff42c50d63999479961a39063e730d0241df5
wasif <wasif.iit.ansari@gmail.com>
2026-08-20 19:41:26 +0530

    Add Spotify API configuration

---
+++ new try/spotify-app/backend/config/spotify.js
@@ -0,0 +1,8 @@
+export const SPOTIFY_API_URL = "https://api.spotify.com/v1";
+export const SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize";
+export const SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token";
+
+export const SPOTIFY_SCOPES = [
+  "user-top-read",
+  "user-read-recently-played",
+].join(" ");
